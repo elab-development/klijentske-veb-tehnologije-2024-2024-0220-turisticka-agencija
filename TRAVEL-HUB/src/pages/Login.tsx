7c4c6ab1fb8927b2b7b499359aee111b1bda9be6
Jovan Orlovic <jovanorlovic.com@gmail.com>
2026-06-18 20:26:16 +0200

    Dodata login stranica

--- TRAVEL-HUB/src/pages/Login.tsx
+++ TRAVEL-HUB/src/pages/Login.tsx
@@ -1,5 +1,10 @@
+import LoginForm from "../components/login/LoginForm"
+
 const Login = () => {
-  return <div></div>;
+  return <section className="w-full min-h-screen flex items-center justify-center px-5 py-10 bg-(--bg-login)">
+      <LoginForm />
+    </section>
+
 };
 
 export default Login;
